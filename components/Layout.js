c3cf18fdbd301626b2507592d611316e2f44515c
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2023-01-10 19:37:08 +0000

    Video-05-Create-Website-Layout

---
+++ components/Layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link legacyBehavior href="/">
+              <a className="text-lg font-bold">Amazona</a>
+            </Link>
+            <div>
+              <Link legacyBehavior href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link legacyBehavior href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright &copy; 2023 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
