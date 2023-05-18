da72cc2b10afb19f49caa8e293fb046c2bc42bec
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2023-05-18 20:36:50 +0100

    Video-23-Pay-Order-By-PayPal

--- README.md
+++ README.md
@@ -82,3 +82,12 @@
     1. implement backend api for order details
     2. load order data from backend
     3. display order details
+22. Create Register Screen
+    1. add signup api
+    2. create register page
+    3. call api on form submit
+23. Pay Order By PayPal
+    1. add paypal button
+    2. handle payment
+    3. create backend api
+    4. update order state
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
 import { SessionProvider, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 export default function App({
   Component,
@@ -10,13 +11,15 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        {Component.auth ? (
-          <Auth>
+        <PayPalScriptProvider deferLoading={true}>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
+          )}
+        </PayPalScriptProvider>
       </StoreProvider>
     </SessionProvider>
   );
